a832f0ab3407ce69da3f9f5579247f97ddf6a389
dil pila <arfren.2013@gmail.com>
2016-09-23 13:31:25 +0545

    typo

--- public/js/main.js
+++ public/js/main.js
@@ -10,57 +10,57 @@ $(document).ready(function () {
     });
 
 
-    $('.form').on('submit', function(e) {
-        e.preventDefault();
-        var data = $(this).serialize();
-        $.ajax({
-            type: "POST",
-            url: '/post',
-            data: data,
-            dataType: "JSON",
-            success: function(res) {
-                if(res.status == 200 && res.success == true) {
-                    swal({
-                        title: 'Success!',
-                        text: res.message,
-                        type: 'success'
-                    }, function (data) {
-                        $('#create').modal('hide');
-                    });
-                }
-            },
-            error: function(err) {
-                swal('Failed!', res.message, 'error');
-            }
-        });
-    });
-
-    // $( ".form" ).submit(function(event) {
-    //     event.preventDefault();
-    
+    // $('.form').on('submit', function(e) {
+    //     e.preventDefault();
     //     var data = $(this).serialize();
-    
     //     $.ajax({
     //         type: "POST",
     //         url: '/post',
-    //         data: data ,
-    
-    //         success: function (data) {
-    //             $("#create").modal('hide');
-    //             $("#mydiv").load(location.href + " #mydiv");
-    //             $(".form")[0].reset();
+    //         data: data,
+    //         dataType: "JSON",
+    //         success: function(res) {
+    //             if(res.status == 200 && res.success == true) {
+    //                 swal({
+    //                     title: 'Success!',
+    //                     text: res.message,
+    //                     type: 'success'
+    //                 }, function (data) {
+    //                     $('#create').modal('hide');
+    //                 });
+    //             }
     //         },
-    //         error: function(xhr, status, error) {
-    //             var data = xhr.responseText;
-    //             var jsonResponse = JSON.parse(data);
-    
-    //             var msg = [];
-    //             $.each(jsonResponse, function(index, value) {
-    //                 msg.push('<li>' + value + '</li>');
-    //             });
-    //             $(".show-div").show();
-    //             $("#errors").html(msg);
-    //         },
+    //         error: function(err) {
+    //             swal('Failed!', res.message, 'error');
+    //         }
     //     });
     // });
+
+    $( ".form" ).submit(function(event) {
+        event.preventDefault();
+    
+        var data = $(this).serialize();
+    
+        $.ajax({
+            type: "POST",
+            url: '/post',
+            data: data ,
+    
+            success: function (data) {
+                $("#create").modal('hide');
+                $("#mydiv").load(location.href + " #mydiv");
+                $(".form")[0].reset();
+            },
+            error: function(xhr, status, error) {
+                var data = xhr.responseText;
+                var jsonResponse = JSON.parse(data);
+    
+                var msg = [];
+                $.each(jsonResponse, function(index, value) {
+                    msg.push('<li>' + value + '</li>');
+                });
+                $(".show-div").show();
+                $("#errors").html(msg);
+            },
+        });
+    });
 });
